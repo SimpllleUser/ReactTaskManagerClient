8374419939ccddc2a74d33204811fb1f480fece9
SimpllleUser <pecheritsa.vv@gmail.com>
2021-10-08 21:51:18 +0300

    init: project client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Card text="Some" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/components/Card.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Card: React.FC<any> = ({ text }: any) => (<h1> {text} </h1>);
+
+export default Card;

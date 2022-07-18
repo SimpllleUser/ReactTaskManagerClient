2fa2126fa3811b71dcb536943c8109531652b4c7
SimpllleUser <pecheritsa.vv@gmail.com>
2022-07-18 20:36:02 +0300

    feat(ProjectDetail): come back edit project

--- src/pages/ProjectDeail.tsx
+++ src/pages/ProjectDeail.tsx
@@ -2,7 +2,7 @@ import { Button, Col, Modal, Row } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { PlusOutlined } from '@ant-design/icons';
+import { PlusOutlined, EditOutlined } from '@ant-design/icons';
 import ProjectForm from '../components/Project/ProjectForm';
 import TaskTable from '../components/Task/TaskTable';
 import { getProjectById } from '../store/project/actions';
@@ -37,23 +37,20 @@ const ProjectDetail: React.FC = () => {
     };
 
     return (<>
-        {/* <Card actions={
-            [
-                <EditOutlined
-                    key="edit"
-                    onClick={showModal} />
-            ]
-        }>
-            <Meta title={title} description={description} />
-            <div>
-                {status?.name}
-            </div>
-        </Card> */}
+
         <Row justify="space-around" align="middle" style={{ padding: '12px 0px' }}>
             <Col span={4}><b>{title}</b></Col>
             <Col span={4}>{description}</Col>
             <Col span={4}>{status?.name}</Col>
-            <Col span={4}><Button  type="primary" icon={<PlusOutlined/>} >Task</Button></Col>
+            <Col span={4}>
+                <Button 
+                type="primary"
+                 onClick={showModal}
+                  icon={<EditOutlined
+                    key="edit"
+                    />} >Edit</Button>
+                <Button type="primary" icon={<PlusOutlined />} >Task</Button>
+            </Col>
         </Row>
         <Row>
             <Modal

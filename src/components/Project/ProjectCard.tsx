c6278aa6362cf402a1779eec9f12082c0950c659
SimpllleUser <pecheritsa.vv@gmail.com>
2022-07-26 21:37:51 +0300

    refactor(project-card): add new style

--- src/components/Project/ProjectCard.tsx
+++ src/components/Project/ProjectCard.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import { Card } from 'antd';
-import {
-    DeleteOutlined
-} from '@ant-design/icons';
-import { ProjectBase } from '../../types';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { deleteProject } from '../../store/project/actions';
-const { Meta } = Card;
+import React from "react";
+import { Button, Card } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
+import { ProjectBase } from "../../types";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deleteProject } from "../../store/project/actions";
+import "../../assets/scss/project-card.scss";
 
 const ProjectCard: React.FC<{ project: ProjectBase }> = ({ project }) => {
-    const dispatch = useDispatch();
-    return (<Card
-        hoverable
-        title={project.title}
-        extra={[<Link to={`project/${project.id}`}>Detail</Link>]}
-        actions={[<DeleteOutlined onClick={() => dispatch(deleteProject(project.id))} />]}
-        style={{ minWidth: '300px' }}
+  const dispatch = useDispatch();
+  return (
+    <Card
+      className="project-card"
+      hoverable
+      title={project.title}
+      extra={[<Link to={`project/${project.id}`}>Detail</Link>]}
+      style={{ minWidth: "300px" }}
     >
-        <Meta description={project.description} />
-    </Card>)
-}
+      <div className="project-card__description">{project.description}</div>
+      <div className="project-card__actions">
+      <Button
+              type="primary"
+              onClick={() => dispatch(deleteProject(project.id))}
+              icon={<DeleteOutlined key="delete"/>}
+              size='small'
+            >Delete</Button>
+      </div>
+      {/* <Meta description={project.description} /> */}
+    </Card>
+  );
+};
 
 export default ProjectCard;

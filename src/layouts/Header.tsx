52342e7d66444f1fa9d40104f0c68ddeb2461041
SimpllleUser <pecheritsa.vv@gmail.com>
2022-07-26 21:18:59 +0300

    feat: add link to home page

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,27 +1,35 @@
-import {Button, PageHeader} from 'antd';
-import React from 'react';
-import {logOut} from "../store/auth/actions";
-import {useDispatch} from "react-redux";
+import { Button, PageHeader } from "antd";
+import React from "react";
+import { logOut } from "../store/auth/actions";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Header: React.FC<any> = () => {
-    const dispatch = useDispatch();
-    const userLogOut = () => {
-        dispatch(logOut());
-    }
+  const dispatch = useDispatch();
+  const userLogOut = () => {
+    dispatch(logOut());
+  };
 
-    return (<div className='header-layout'>
-        <PageHeader
-            ghost={false}
-            title="My task manager"
-            extra={[
-                <Button key="1" type="primary" danger onClick={() => userLogOut()}>
-                    Logout
-                </Button>,
-            ]}
-            style={{backgroundColor: 'var(--antd-wave-shadow-color)'}}
-        >
-        </PageHeader>
-    </div>)
+  return (
+    <div className="header-layout">
+      <PageHeader
+        ghost={false}
+        title={
+          <Button type="primary">
+            <Link to="/" color="red">
+              <b> My task manager </b>
+            </Link>
+          </Button>
+        }
+        extra={[
+          <Button key="1" type="primary" danger onClick={() => userLogOut()}>
+            Logout
+          </Button>,
+        ]}
+        style={{ backgroundColor: "var(--antd-wave-shadow-color)" }}
+      ></PageHeader>
+    </div>
+  );
 };
 
 export default Header;

b253b411bc24d7533d7db874e901aaf9c6b7a61a
simpllleser <pecherytsia.vv@gmail.com>
2022-12-05 13:39:25 +0200

    feat: add layout

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
-import Header from "./Header";
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  FolderOutlined,
+  UnorderedListOutlined,
+} from '@ant-design/icons';
+import './BaseLayout.css';
 
-const BaseLayout: React.FC<any> = ({children}) => (<div className='base-layout'>
-    <Header/>
-    {children}
-</div> );
+const { Header, Sider, Content } = Layout;
+
+const BaseLayout: React.FC = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout style={{ height: '90vh' }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">Task manager</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: 'project',
+              icon: <FolderOutlined />,
+              label: 'Projects',
+            },
+            {
+              key: 'task',
+              icon: <UnorderedListOutlined  />,
+              label: 'Tasks',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+        <div style={{ width: '1rem' }}>
+        {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+            className: 'trigger',
+            onClick: () => setCollapsed(!collapsed),
+          })}
+        </div>
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '8px 16px',
+            padding: 8,
+            minHeight: 280,
+          }}
+        >
+          { children }
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
 export default BaseLayout;

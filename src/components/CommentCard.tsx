b5cc4e9a7800ee24d3767c63c7df6ae8a8ed5310
simpllleser <pecherytsia.vv@gmail.com>
2022-12-22 16:53:19 +0200

    feat(CommentCard): style

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Avatar, Comment, Tooltip } from 'antd';
+import { Comment } from 'antd';
+import { FileImageOutlined } from "@ant-design/icons";
+
 import { ProjectComment } from '../types';
 
 const CommentCard: React.FC<any> = ({ comment }: { comment: ProjectComment }) => (<>
     <Comment
-        content={comment.body}
-        author={comment.author.name}
+        content={<p style={{ textAlign: 'start' }}>
+            {comment.body}
+        </p>}
+        avatar={ <FileImageOutlined style={{fontSize: '2rem'}} /> }
+        author={
+            <div>
+                <b>{comment.author.name}</b>
+            </div>
+        }
         datetime={new Date(comment.createdAt).toLocaleDateString()}
     />
+    <hr />
 </>);
 
 export default CommentCard;
